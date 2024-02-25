b03e1d94ad5f9cf0ddd3b9fd63068a7ca5a2ffac
ValeriySVV <svv1986@ukr.net>
2024-02-25 12:04:13 +0200

    First version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,8 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -21,7 +23,11 @@ import java.util.stream.Stream;
 
 public class Main {
 
+    private static Logger logger;
+
     private static final String FILE_NAME = "resources/samsungModels.txt";
+
+    private static final String DELIMITER = ";";
     private static java.util.stream.Collectors Collectors;
 
     static String[] readFileUsingScanner(String fileName) {
@@ -62,40 +68,61 @@ public class Main {
     }
 
     private static void getSamsungsData(String[] data, List<Samsung> samsungs) {
+        System.setProperty("java.util.logging.config.file",System.getenv("LOGGER_CONFIG"));
+
+
+        logger = Logger.getLogger(Main.class.getName());
+        logger.log(Level.CONFIG, "Зчитування даних з файлу " + FILE_NAME);
+
+
+
         var start = System.currentTimeMillis();
         //List<Samsung> samsungs = new LinkedList<>();
         //getSamsungData(data, samsungs);
         //Stream<Samsung> stream = samsungs.stream();
         for (int i = 1; i < data.length; i++) {
-            var pieces = data[i].split(";");
+            //var pieces = data[i].split(DELIMITER);
             Samsung samsung = null;
-            //System.out.println(data);
-            switch (pieces[4].toLowerCase()) {
-                case "galaxyS":
-                    samsung = new GalaxyA(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
-                            Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
-                    break;
-                case "galaxyM":
-                    samsung = new GalaxyFlip(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
-                            Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
-                    break;
-                case "galaxyA":
-                    samsung = new GalaxyM(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
-                            Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
-                    break;
-                case "galaxyFlip":
-                    samsung = new GalaxyS(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
-                            Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
-                    break;
-                default:
-                    samsung = new Samsung(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
-                            Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
-                    break;
+            if (i > 0) {
+                var pieces = data[i].split(DELIMITER);
+                //System.out.println(pieces);
 
+                //System.out.println(data);
+                switch (pieces[4].toLowerCase()) {
+                    case "galaxyS":
+                        samsung = new GalaxyS(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
+                                Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
+                        logger.log(Level.WARNING, "Створення моделі телефону з класу galaxyS: " + samsung);
+                        break;
+                    case "galaxyM":
+                        samsung = new GalaxyM(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
+                                Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
+                        logger.log(Level.WARNING, "Створення моделі телефону з класу galaxyM: " + samsung);
+                        break;
+                    case "galaxyA":
+                        samsung = new GalaxyA(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
+                                Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
+                        logger.log(Level.WARNING, "Створення моделі телефону з класу galaxyA: " + samsung);
+                        break;
+                    case "galaxyFlip":
+                        samsung = new GalaxyFlip(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
+                                Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
+                        logger.log(Level.INFO, "Створення моделі телефону з класу galaxyFlip: " + samsung);
+                        break;
+                    default:
+                        samsung = new Samsung(Integer.parseInt(pieces[0]), pieces[1], pieces[2], Integer.parseInt(pieces[3]),
+                                Integer.parseInt(pieces[5]), Boolean.parseBoolean(pieces[6]));
+                        logger.log(Level.SEVERE, "Створення моделі телефону з класу galaxy: " + samsung);
+                        logger.log(Level.WARNING, "Створення моделі телефону з класу galaxy: " + samsung);
+                        logger.log(Level.INFO, "Створення моделі телефону з класу galaxy: " + samsung);
+                        logger.log(Level.FINE, "Створення моделі телефону з класу galaxy: " + samsung);
+                        logger.log(Level.FINER, "Створення моделі телефону з класу galaxy: " + samsung);
+                        break;
+
+                }
+                samsungs.add(samsung);
+                System.out.println("Phone characteristics:" + samsung);
             }
-            samsungs.add(samsung);
-            System.out.println("Phone characteristics:" + samsung);
-
         }
         System.out.println();
         System.out.println("Імпорт даних із файлу становить " + (System.currentTimeMillis() - start));
